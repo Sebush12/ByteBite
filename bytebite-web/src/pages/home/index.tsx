fc72ec1e3786cbe6fade26f99defdca4a90462d6
sebgor1012 <gora1s@cmich.edu>
2023-10-02 10:38:08 -0400

    fet(react): created homepage

---
+++ bytebite-web/src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FC } from "react";
+
+export const Home: FC = () => { 
+    return (
+        <>
+        <h1>
+            ByteBite-Tracker
+        </h1>
+        </>
+    )
+}
+export default Home;
