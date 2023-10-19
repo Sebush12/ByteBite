d63a4f659af4492915f0f48cdd89a58f0824de4a
wilts <wiltsie.connor@gmail.com>
2023-10-19 17:06:37 -0400

    Created schemas for database

--- api/bytebite/models.py
+++ api/bytebite/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
+from django.shortcuts import render
+from django.db.models import Sum
 
 
 # Create your models here.
 # Django creates an id (primary key) automatically
 class Users(models.Model):
+    username = models.CharField(max_length=40)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     name = models.CharField(max_length=40, blank=True)
@@ -31,10 +34,49 @@ class Users_info(models.Model):
         Users, related_name="info", on_delete=models.CASCADE, primary_key=True
     )
     height = models.IntegerField()
+    age = models.PositiveIntegerField()
     start_weight = models.DecimalField(max_digits=5, decimal_places=2)
     weight = models.DecimalField(max_digits=5, decimal_places=2)
     goal_weight = models.DecimalField(max_digits=5, decimal_places=2)
-    weekly_goal = models.IntegerField()
+    daily_calories = models.IntegerField()
+    goal = models.CharField(255)
 
     def __str__(self):
         return f"Info for {self.user.first_name} {self.user.last_name}"
+    
+class FoodItem(models.Model):
+    name = models.CharField(max_length=255, primary_key=True)
+    calories = models.PostiveIntegerField()
+    protein = models.PostiveDecimalField(max_digits=5, decimal_places=2)
+    carbs = models.PositiveDecimalField(max_digits=5, decimal_places=2)
+    fat = models.PositiveDecimalField(max_digits=5, decimal_places=2)
+
+
+class UserFoodLog(models.Model):
+    user = models.ForeignKey(Users_info, on_delete=models.CASCADE)
+    date = models.DateField()
+    food_item = models.ForeignKey(FoodItem, on_delete=models.CASCADE)
+    servings = models.PositiveIntegerField()
+
+def user_food_log_summary(request, user_id, date):
+    # Retrieve the user's profile
+    user_profile = Users_info.objects.get(user_id=user_id)
+
+    # Query all food items logged by the user for the specified date
+    user_food_logs = UserFoodLog.objects.filter(user=user_profile, date=date)
+
+    # Calculate the total macros intake for the day
+    total_calories = user_food_logs.aggregate(total_calories=Sum('food_itemcalories'))['total_calories']
+    total_protein = user_food_logs.aggregate(total_protein=Sum('food_itemprotein'))['total_protein']
+    total_carbohydrates = user_food_logs.aggregate(total_carbohydrates=Sum('food_itemcarbohydrates'))['total_carbohydrates']
+    total_fat = user_food_logs.aggregate(total_fat=Sum('food_itemfat'))['total_fat']
+
+    context = {
+        'user_profile': user_profile,
+        'date': date,
+        'user_food_logs': user_food_logs,
+        'total_calories': total_calories,
+        'total_protein': total_protein,
+        'total_carbohydrates': total_carbohydrates,
+        'total_fat': total_fat,
+    }
