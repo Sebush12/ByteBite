a9504f519c471f253acf98f4ac946e316ecab8a7
Alex M. Flynn <106895945+AlexMFlynn@users.noreply.github.com>
2023-10-22 16:50:19 -0400

    feat: updated ui and nav components (#13)

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -1,8 +1,5 @@
 import { ColorModeScript, theme } from "@chakra-ui/react";
 import { Html, Head, Main, NextScript } from "next/document";
-import { useRouter } from "next/router";
-import { useState } from "react";
-import { SelectedPage } from "./shared/types";
 
 export default function Document() {
   return (
