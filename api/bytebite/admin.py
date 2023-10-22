63e9e9b3924d82362bfe14acd174139493546b58
wilts <wiltsie.connor@gmail.com>
2023-10-22 14:05:19 -0400

    registered UserFoodLog and FoodItem models

--- api/bytebite/admin.py
+++ api/bytebite/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from bytebite.models import Users, Users_info
+from bytebite.models import Users, Users_info, UserFoodLog, FoodItem
 
 # Register your models here.
 admin.site.register(Users)
 admin.site.register(Users_info)
+admin.site.register(UserFoodLog)
+admin.site.register(FoodItem)
