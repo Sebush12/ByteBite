e17875029dfd0b114d1e08b7ca29dda24d0797ab
Sebastian Gora <146003329+sebgor1012@users.noreply.github.com>
2023-11-06 10:03:35 -0500

    Finished visual on page (#35)
    Finished up the boxes we needed for all the information. From here, we need to make the avatar editable, and all the text boxes as well. Some will pull info from calculations. Others will be something user can change.
    
    Co-authored-by: Alex M. Flynn <106895945+AlexMFlynn@users.noreply.github.com>

--- web/src/components/ui/account.tsx
+++ web/src/components/ui/account.tsx
@@ -41,45 +41,92 @@ export const Account: FC = () => {
               </Box>
               <Box pb={5}>
                 <Text>Caloric Goal</Text>
-                <Textarea
+                <Input
+                  value="Your Caloric Goal" // Add your caloric goal value here
                   isReadOnly={true}
-                  placeholder="Here is a sample placeholder"
-                  size="mg"
+                  variant="outline"
+                />
+              </Box>
+              <Box pb={5}>
+                <Text>Weight</Text>
+                <Input
+                  value="Your Weight" // Add your weight value here
+                  isReadOnly={true}
+                  variant="outline"
+                />
+              </Box>
+              <Box pb={5}>
+                <Text>Weight Goal</Text>
+                <Input
+                  value="Your Weight Goal" // Add your weight goal value here
+                  isReadOnly={true}
                   variant="outline"
                 />
               </Box>
             </SimpleGrid>
           </CardBody>
-          <CardFooter />
+          <CardFooter>
+            <Button variant="outline" alignSelf="flex-end">
+              Save
+            </Button>
+          </CardFooter>
         </Card>
       </Box>
 
       <Box margin={1}>
-        <Card align="center">
+        <Card align="left">
           <CardHeader>
-            <Heading size="md"> Customer dashboard</Heading>
+            <Heading size="md" padding={2}>
+              Account Settings
+            </Heading>
           </CardHeader>
           <CardBody>
-            <Text>
-              View a summary of all your customers over the last month.
-            </Text>
+            <SimpleGrid columns={2} spacing={5}>
+              <Box pb={5}>
+                <Text>Display Name</Text>
+                <Input
+                  value="Your Display Name" // Add your display name here
+                  isReadOnly={true}
+                  variant="outline"
+                />
+              </Box>
+              <Box pb={5}>
+                <Text>Username</Text>
+                <Input
+                  value="Your Username" // Add your username here
+                  isReadOnly={true}
+                  variant="outline"
+                />
+              </Box>
+              <Box pb={5}>
+                <Text>Email</Text>
+                <Input
+                  value="Your Email" // Add your email here
+                  isReadOnly={true}
+                  variant="outline"
+                />
+              </Box>
+              <Box pb={5}>
+                <Text>Phone Number</Text>
+                <Input
+                  value="Your Phone Number" // Add your phone number here
+                  isReadOnly={true}
+                  variant="outline"
+                />
+              </Box>
+              <Box pb={5}>
+                <Button variant="outline">Change Password</Button>
+              </Box>
+            </SimpleGrid>
           </CardBody>
-          <CardFooter />
+          <CardFooter>
+            <Button variant="outline" alignSelf="flex-end">
+              Save
+            </Button>
+          </CardFooter>
         </Card>
       </Box>
     </>
-
-  // <Box height={"90vh"}>
-  //   <Flex justifyContent="flex-start" alignItems="center" p={4}>
-  //     <Box></Box>
-  //     <Stack spacing={2} align="left">
-  //       <Avatar size={"2xl"} src={avatarURL} />
-  //     </Stack>
-  //     <Stack spacing={2} align="center" padding={4}>
-  //       <Heading as="h1">Account Settings</Heading>
-  //     </Stack>
-  //   </Flex>
-  // </Box>
   );
 };
 
