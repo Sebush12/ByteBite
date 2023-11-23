9fa42fc3261829e4479bf46ccf561ea538b7469b
Alex Flynn <aflynn501@gmail.com>
2023-11-22 20:42:29 -0500

    fix: add mutation logic

---
+++ web/src/gql/mutation-login.ts
@@ -0,0 +1,34 @@
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+import { Exact } from './graphql';
+import { gql } from 'urql';
+
+export type LoginUserMutationVariables = Exact<{
+  username: string;
+  password: string;
+}>;
+
+export interface LoginUserMutation {
+  loginUser: {
+    users: {
+      id: string;
+      username: string;
+      firstName: string;
+      lastName: string;
+      email: string;
+    } | null;
+  };
+}
+
+export const LOGIN_USER: DocumentNode<LoginUserMutation, LoginUserMutationVariables> = gql`
+  mutation LoginUser($username: String!, $password: String!) {
+    loginUser(username: $username, password: $password) {
+      users {
+        id
+        username
+        firstName
+        lastName
+        email
+      }
+    }
+  }
+`;
