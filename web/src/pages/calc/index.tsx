567af79415da17468384c889b9c18365094801ac
Alex Flynn <aflynn501@gmail.com>
2023-11-28 19:44:01 -0500

    fix: fixed calc page not loading

--- web/src/pages/calc/index.tsx
+++ web/src/pages/calc/index.tsx
@@ -1,24 +1,24 @@
-import { Flex, Stack } from "@chakra-ui/react";
-import React from "react";
+import { Flex, Stack } from '@chakra-ui/react';
+import React from 'react';
 
-import { CalorieCalc } from "@/components/calcs/cal-intake";
-import { TdeeCalc } from "@/components/calcs/tdee-calc";
-import { BmiCalc } from "@/components/calcs/bmi-calc";
-import { useSession } from "next-auth/react";
-import LoginReq from "../login";
+import { CalorieCalc } from '@/components/calcs/cal-intake';
+import { TdeeCalc } from '@/components/calcs/tdee-calc';
+import { BmiCalc } from '@/components/calcs/bmi-calc';
+import { useSession } from 'next-auth/react';
+import LoginReq from '@/components/forms/login-required';
 
 const CalorieIntakeCalculator: React.FC = () => {
   const { status } = useSession();
-  if (status == "unauthenticated") {
+  if (status == 'unauthenticated') {
     return <LoginReq />;
   }
 
   return (
-    <Flex align={"center"} justify={"center"}>
+    <Flex align={'center'} justify={'center'}>
       <Stack
         spacing={4}
         p={4}
-        direction={{ base: "column", md: "row" }}
+        direction={{ base: 'column', md: 'row' }}
         align="center"
       >
         <BmiCalc />
