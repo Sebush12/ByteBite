dc73f7ec15aad0aff33e2781d75bc13b4f5dcd22
Alex Flynn <aflynn501@gmail.com>
2023-11-29 10:48:14 -0500

    fix: updated login required logic

--- web/src/components/forms/login-required.tsx
+++ web/src/components/forms/login-required.tsx
@@ -11,14 +11,48 @@ import {
   useColorModeValue,
   Link,
   Stack,
-  Text
+  Text,
+  useToast
 } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 import { FC, useState } from 'react';
+import { useFormik } from 'formik';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 export const LoginReq: FC = () => {
+  const [loginError, setLoginError] = useState(false);
+  const toast = useToast();
+  const router = useRouter();
   const [showPassword, setShowPassword] = useState(false);
+
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    onSubmit: async (values) => {
+      const response = await signIn('credentials', {
+        email: values.email,
+        password: values.password,
+        redirect: false
+      });
+      if(response?.error) {
+        setLoginError(true);
+        toast({
+          title: 'Invalid email or password',
+          status: 'error',
+          isClosable: true,
+          position: 'top'
+        });
+      } else {
+        router.push('/calc');
+      }
+    }
+  });
+
   return (
     <Flex align={'center'} justify={'center'} height="89vh">
       <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
@@ -33,49 +67,64 @@ export const LoginReq: FC = () => {
           boxShadow={'lg'}
           p={8}
         >
-          <Stack spacing={4}>
-            <FormControl id="userName" isRequired>
-              <FormLabel>Username</FormLabel>
-              <Input type="text" />
-            </FormControl>
-            <FormControl id="password" isRequired>
-              <FormLabel>Password</FormLabel>
-              <InputGroup>
-                <Input type={showPassword ? 'text' : 'password'} />
-                <InputRightElement h={'full'}>
-                  <Button
-                    variant={'ghost'}
-                    onClick={(): void =>
-                      setShowPassword((showPassword: boolean) => !showPassword)
-                    }
-                  >
-                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-            </FormControl>
-            <Stack spacing={10} pt={2}>
-              <Button
-                loadingText="Submitting"
-                size="lg"
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500'
-                }}
-              >
+          <form onSubmit={formik.handleSubmit}>
+            <Stack spacing={4}>
+              <FormControl isRequired isInvalid={loginError}>
+                <FormLabel htmlFor='email'>Email</FormLabel>
+                <Input
+                  type="text"
+                  onChange={formik.handleChange}
+                  value={formik.values.email}
+                  id="email"
+                  name="email"
+                />
+              </FormControl>
+              <FormControl isRequired isInvalid={loginError}>
+                <FormLabel htmlFor='password'>Password</FormLabel>
+                <InputGroup>
+                  <Input
+                    type={showPassword ? 'text' : 'password'}
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                    id="password"
+                    name="password"
+                  />
+                  <InputRightElement h={'full'}>
+                    <Button
+                      variant={'ghost'}
+                      onClick={(): void =>
+                        setShowPassword((showPassword: boolean) => !showPassword)
+                      }
+                    >
+                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+              <Stack spacing={10} pt={2}>
+                <Button
+                  type='submit'
+                  loadingText="Submitting"
+                  size="lg"
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500'
+                  }}
+                >
                 Login
-              </Button>
-            </Stack>
-            <Stack pt={6}>
-              <Text align={'center'}>
+                </Button>
+              </Stack>
+              <Stack pt={6}>
+                <Text align={'center'}>
                 Not a user?{' '}
-                <Link as={NextLink} color={'blue.400'} href='/new-user'>
+                  <Link as={NextLink} color={'blue.400'} href='/new-user'>
                   Signup
-                </Link>
-              </Text>
+                  </Link>
+                </Text>
+              </Stack>
             </Stack>
-          </Stack>
+          </form>
         </Box>
       </Stack>
     </Flex>
